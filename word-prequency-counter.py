894716070292127c5e8f7c9348f68224ba064a72
Bui Cong Thanh <congthanhbkit03@gmail.com>
2018-10-05 15:27:42 +0700

    Hoan thanh file word-prequency-counter.py

--- word-prequency-counter.py
+++ word-prequency-counter.py
@@ -13,8 +13,30 @@ def start(url):
 		# print(content)
 		words = content.lower().split()
 		for eword in words:
-			print (eword)
+			# print (eword)
 			wordlist.append(eword)
+	clean_up_list(wordlist)
+
+def clean_up_list(wordlist):
+	clear_word_list = []
+	for word in wordlist:
+		sysbols = "!?.@#@%&_+:\"“”<>?./;'[]-=',"
+		for i in range(0, len(sysbols)):
+			word = word.replace(sysbols[i], "")
+		if(len(word) > 0):
+			# print(word)
+			clear_word_list.append(word)
+	create_dictionary(clear_word_list)
+
+def create_dictionary(clear_word_list):
+	word_count = {}
+	for word in clear_word_list:
+		if (word in word_count):
+			word_count[word] += 1
+		else:
+			word_count[word] = 1
+	for key, value in sorted(word_count.items(), key=operator.itemgetter(1)):
+		print(key, value)
 start('https://dantri.com.vn/the-gioi.htm')
 # start('http://hdsieunhanh.com/filter/trang-1.html')
 
